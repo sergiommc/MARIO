65d53d4fdda458524e70eed0ec4bcd6b8fd36dff
sergiommc <sergiomatosmoreira@hotmail.com>
2014-11-28 22:14:44 -0300

    Create Galo.java

---
+++ Galo.java
@@ -0,0 +1,13 @@
+
+package animais;
+
+
+public class Galo extends Animal {
+        public Galo(String n){
+            super(n);
+            caminhada = "2 ";
+        }
+        public String getAndando(){
+        return caminhada;
+    }
+}
